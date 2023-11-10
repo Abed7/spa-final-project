9b0d725bdf1de57829f34d2ad4b6c1ffdc13d0c5
Torben Bis <bistorben@gmail.com>
2023-11-10 11:03:23 +0100

    api testing

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,31 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState([{ image: "" }]);
+
   useEffect(() => {
     const apiKey = "6068b5348e854d27add8cc069010b8dd";
-    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`)
+    const apiKeyO = "c7250b0a541c49f0ad7cde17c064bb04";
+    const apiKeyN = "fcdf6ca97b5c4a468a70e2bbb8bd0bbf";
+    fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${apiKeyO}&number=30`
+    )
       .then((response) => response.json())
       .then((json) => {
-        console.log(json.results[0].image);
-        console.log(json.results);
+        // console.log(json.results[0].image);
+        console.log(json);
         // console.log(json.results[0]);
-        setData(json.results);
+        setData(json);
       });
   }, []);
 
   return (
     <>
       <Navbar />
-      <img src={data[0].image} alt="" />
+      {/* {data.map((recipe) => (
+        <img src={recipe.image} />
+      ))} */}
+
       <Home />
     </>
   );
