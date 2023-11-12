c007dc4c124f3068b3b6a0f779781981a03e3d33
Torben Bis <bistorben@gmail.com>
2023-11-12 14:21:43 +0100

    still api testing

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,36 +1,51 @@
 import { useState, useEffect } from "react";
 import "./Explore.css";
+import { FaSistrix } from "react-icons/fa6";
 
 const Explore = () => {
   const [data, setData] = useState([]);
   const [isDataLoaded, setIsDataLoaded] = useState(false);
+  const [search, setSearch] = useState("");
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setSearch(e.target.value);
+  };
 
   useEffect(() => {
     const apiKey = "6068b5348e854d27add8cc069010b8dd";
     const apiKeyO = "c7250b0a541c49f0ad7cde17c064bb04";
     const apiKeyN = "fcdf6ca97b5c4a468a70e2bbb8bd0bbf";
     fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${apiKey}&number=30`
+      `https://api.spoonacular.com/recipes/random?apiKey=${apiKeyO}&number=30`
     )
+      // fetch(
+      //   `https://api.spoonacular.com/recipes/search?query=burger?apiKey=${apiKey}&number=30`
+      // )
       .then((response) => response.json())
       .then((json) => {
         // console.log(json.results[0].image);
-        console.log(json.recipes);
+
         // console.log(json.results[0]);
         setData(json.recipes);
         setIsDataLoaded(true);
       });
   }, []);
-  console.log(data);
 
   return (
     <main className="Explore">
       <section className="stage-explpore"></section>
       <section className="search-filter-wrapper">
-        <div className="search-wrapper">
-          <input type="text" />
-          <button>ICON</button>
-        </div>
+        <form onSubmit={submitHandler} className="search-wrapper">
+          <input type="text" onChange={searchHandler} value={search}></input>
+          <button>
+            <FaSistrix />
+          </button>
+        </form>
       </section>
       <section className="search-results">
         {isDataLoaded
