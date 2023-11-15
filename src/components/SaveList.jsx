cae64354269012a496d41e56fa1c051d2d699d98
Munem Soufi <munem.soufi@gmail.com>
2023-11-15 15:48:54 +0100

    save item function

--- src/components/SaveList.jsx
+++ src/components/SaveList.jsx
@@ -1,14 +1,25 @@
 import { useNavigate } from "react-router-dom";
 import { useAuth } from "./Auth";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import "./SaveList.css";
 
 const SaveList = () => {
+    const [saveItems, setSeveItems] = useState(['Item 1', 'Item 2', 'Item 3']);
+
   const auth = useAuth();
   const navigate = useNavigate();
   
 
 
+  useEffect(() => {
+    
+    console.log('Watched items updated:', saveItems);
+    // You can perform additional actions here, such as updating the UI
+  }, [saveItems]);
+
+  const addToSaveList = newItem => {
+    setSeveItems(prevItems => [...prevItems, newItem]);
+  };
 
   const handleLogout = () => {
     auth.logout();
@@ -22,7 +33,12 @@ const SaveList = () => {
       <div>SaveList</div>
       <h5>Welcome {auth.user}</h5>
       <button onClick={handleLogout}>Logout</button>
-
+      <ul>
+        {saveItems.map(item => (
+          <li key={item}>{item}</li>
+        ))}
+      </ul>
+      <button onClick={() => addToSaveList('New Item')}>Add to List</button>
     </section>
   );
 };
